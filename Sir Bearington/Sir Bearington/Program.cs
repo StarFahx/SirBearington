473b3e5834807d08ef9c56547f8c4f6319381449
James Fox <jamesfoxma@gmail.com>
2017-09-17 15:49:49 +0100

    Update to .NET 4.6.1 and add initial async framework

--- Sir Bearington/Sir Bearington/Program.cs
+++ Sir Bearington/Sir Bearington/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace Sir_Bearington
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+        
+        public async Task MainAsync()
         {
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
